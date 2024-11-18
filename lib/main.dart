57f3dc7e686d9d7cf2c163d111ae52cd6278539a
Edouard Marquez <moi@edouard-marquez.me>
2024-11-18 18:13:08 +0100

    SPider avec SVG + couleurs

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/res/app_colors.dart';
 import 'package:flutter_application_1/res/app_images.dart';
 
 void main() {
@@ -13,8 +14,53 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        colorScheme: ColorScheme.light(primary: AppColors.blue),
+        primaryColor: AppColors.blue,
+        primaryColorLight: AppColors.blueLight,
+        primaryColorDark: AppColors.blueDark,
+        fontFamily: 'Avenir',
+        appBarTheme: const AppBarTheme(
+          centerTitle: false,
+          backgroundColor: AppColors.white,
+          elevation: 0.0,
+          iconTheme: IconThemeData(
+            color: AppColors.blue,
+          ),
+          titleTextStyle: TextStyle(
+            color: AppColors.blue,
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            color: AppColors.blue,
+            fontSize: 22.0,
+            fontWeight: FontWeight.bold,
+          ),
+          displayMedium: TextStyle(
+            color: AppColors.gray2,
+            fontSize: 18.0,
+          ),
+          headlineMedium: TextStyle(
+            color: AppColors.gray3,
+            fontSize: 17.0,
+          ),
+          headlineSmall: TextStyle(
+            color: AppColors.blue,
+            fontSize: 17.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        dividerColor: AppColors.gray2,
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          showSelectedLabels: true,
+          showUnselectedLabels: true,
+          selectedItemColor: AppColors.blue,
+          unselectedItemColor: AppColors.gray2,
+          type: BottomNavigationBarType.fixed,
+        ),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
