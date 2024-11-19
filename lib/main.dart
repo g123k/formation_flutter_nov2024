a61182d06e338f3f2751586a0f8b3ce1b55c1264
Edouard Marquez <moi@edouard-marquez.me>
2024-11-19 10:57:40 +0100

    Correction 1er écran

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/res/app_colors.dart';
-import 'package:flutter_application_1/res/app_images.dart';
+import 'package:flutter_application_1/screens/homepage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
         colorScheme: ColorScheme.light(primary: AppColors.blue),
@@ -62,56 +63,7 @@ class MyApp extends StatelessWidget {
           type: BottomNavigationBarType.fixed,
         ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset(AppImages.nutriscoreA),
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: HomePage(),
     );
   }
 }
---
+++ lib/screens/homepage.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/res/app_colors.dart';
+import 'package:flutter_application_1/res/app_icons.dart';
+import 'package:flutter_application_1/res/app_vectorial_images.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+/// Avec Flex
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Mes scans'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Padding(
+              padding: const EdgeInsetsDirectional.only(end: 7.0),
+              child: Icon(AppIcons.barcode),
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Spacer(flex: 20),
+            SvgPicture.asset(AppVectorialImages.illEmpty),
+            Spacer(flex: 12),
+            Text('Vous n\'avez pas encore scanné de produit'),
+            Spacer(flex: 8),
+            TextButton(
+              style: OutlinedButton.styleFrom(
+                foregroundColor: AppColors.blue,
+                backgroundColor: AppColors.yellow,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(22.0),
+                  ),
+                ),
+              ),
+              onPressed: () {},
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text('Commencer'.toUpperCase()),
+                  const SizedBox(width: 10.0),
+                  Icon(Icons.arrow_right_alt),
+                ],
+              ),
+            ),
+            Spacer(flex: 20),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/// Avec LayoutBuilder + FittedBox
+class HomePage2 extends StatelessWidget {
+  const HomePage2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Mes scans'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Padding(
+              padding: const EdgeInsetsDirectional.only(end: 7.0),
+              child: Icon(AppIcons.barcode),
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: LayoutBuilder(builder: (context, BoxConstraints constraints) {
+          return FittedBox(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SvgPicture.asset(AppVectorialImages.illEmpty),
+                SizedBox(height: constraints.maxHeight * 0.15),
+                Text('Vous n\'avez pas encore scanné de produit'),
+                SizedBox(height: constraints.maxHeight * 0.1),
+                TextButton(
+                  style: OutlinedButton.styleFrom(
+                    foregroundColor: AppColors.blue,
+                    backgroundColor: AppColors.yellow,
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(22.0),
+                      ),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text('Commencer'.toUpperCase()),
+                      const SizedBox(width: 10.0),
+                      Icon(Icons.arrow_right_alt),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
+
+class FittedBoxDemo extends StatelessWidget {
+  const FittedBoxDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FittedBox(
+        child: Text(
+          'Hello world',
+          style: TextStyle(fontSize: 1000),
+        ),
+      ),
+    );
+  }
+}
